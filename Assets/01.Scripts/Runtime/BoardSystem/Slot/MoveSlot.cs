c1b36a418979d727327f5372267e1d70054e9966
LEE SANG GYU <tkdrb062@gmail.com>
2025-10-01 17:09:49 +0900

    feat: Move Slot

--- Assets/01.Scripts/Runtime/BoardSystem/Slot/MoveSlot.cs
+++ Assets/01.Scripts/Runtime/BoardSystem/Slot/MoveSlot.cs
@@ -1,6 +1,64 @@
+using System;
+using UnityEngine;
+
 namespace MIE.Runtime.BoardSystem.Slot
 {
     public class MoveSlot : BaseSlot
     {
+        [Header("Movement Settings")]
+        [SerializeField] private Vector2 moveDirection = Vector2.right;
+        [SerializeField] private float moveSpeed = 100f;
+        
+        [Header("Infinite Scroll Bounds")]
+        [SerializeField] private float leftBound = -1000f;
+        [SerializeField] private float rightBound = 1000f;
+        [SerializeField] private float topBound = 600f;
+        [SerializeField] private float bottomBound = -600f;
+
+
+        private void Update()
+        {
+            Move();
+            CheckBoundsAndWrap();
+        }
+
+        private void Move()
+        {
+            Vector3 newPosition = transform.localPosition + (Vector3)moveDirection * moveSpeed * Time.deltaTime;
+            transform.localPosition = newPosition;
+        }
+
+        private void CheckBoundsAndWrap()
+        {
+            Vector3 currentPos = transform.localPosition;
+            bool needsWrap = false;
+
+            if (moveDirection.x > 0 && currentPos.x > rightBound)
+            {
+                currentPos.x = leftBound;
+                needsWrap = true;
+            }
+            else if (moveDirection.x < 0 && currentPos.x < leftBound)
+            {
+                currentPos.x = rightBound;
+                needsWrap = true;
+            }
+
+            if (moveDirection.y > 0 && currentPos.y > topBound)
+            {
+                currentPos.y = bottomBound;
+                needsWrap = true;
+            }
+            else if (moveDirection.y < 0 && currentPos.y < bottomBound)
+            {
+                currentPos.y = topBound;
+                needsWrap = true;
+            }
+
+            if (needsWrap)
+            {
+                transform.localPosition = currentPos;
+            }
+        }
     }
 }
